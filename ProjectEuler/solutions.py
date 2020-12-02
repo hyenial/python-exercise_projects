17f8512298048efb16e07a5af8b454bd059d809d
Harun Yenial <harun.yenial@gmail.com>
2020-12-02 00:02:16 -0500

    Update solutions.py

--- ProjectEuler/solutions.py
+++ ProjectEuler/solutions.py
@@ -1,3 +1,5 @@
+# 
+# Referance: https://wiki.python.org/moin/ProblemSets/Project%20Euler%20Solutions
 # Problem 1: Add all the natural numbers below 1000 that are multiples of 3 or 5.
 # http://projecteuler.net/index.php?section=problems&id=1
     def multiples_of_3_or_5():
