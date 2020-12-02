4bc8d78db0ca61195140c9a65fe1800d675d6d59
Harun Yenial <harun.yenial@gmail.com>
2020-12-01 23:55:18 -0500

    Create solutions.py

---
+++ ProjectEuler/solutions.py
@@ -0,0 +1,13 @@
+# Problem 1: Add all the natural numbers below 1000 that are multiples of 3 or 5.
+# http://projecteuler.net/index.php?section=problems&id=1
+    def multiples_of_3_or_5():
+        for number in xrange(1000):
+            if not number % 3 or not number % 5:
+                yield number
+
+    print sum(multiples_of_3_or_5())
+    
+# In one line:
+    print sum( number for number in xrange(1000) if not (number % 3 and number % 5) )
+
+    
