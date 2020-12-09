3e58739341a316ea857b2b3587cd9ad78dc7e6e4
Harun Yenial <harun.yenial@gmail.com>
2020-12-09 00:01:51 -0500

    Update solution_7.py

--- ProjectEuler/solution_7.py
+++ ProjectEuler/solution_7.py
@@ -3,15 +3,24 @@
 What is the 10 001st prime number?
 '''
 
-import eulerlib, itertools
+def isPrime(n):
+    if n < 2: return "Neither prime, nor composite"
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-def compute():
-	ans = next(itertools.islice(filter(eulerlib.is_prime, itertools.count(2)), 10000, None))
-	return str(ans)
+# returns the nth prime number
+def nthPrime(n):
+    numberOfPrimes = 0
+    prime = 1
 
+    while numberOfPrimes < n:
+        prime += 1
+        if isPrime(prime):
+            numberOfPrimes += 1
+    return prime
 
-if __name__ == "__main__":
-	print(compute())
-  
+print(nthPrime(10001))
   
 # referance : https://github.com/nayuki/Project-Euler-solutions
