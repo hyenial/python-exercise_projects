e53a7f28720b7d5b47ae30ff22c61507ab12aee3
Harun Yenial <harun.yenial@gmail.com>
2020-12-15 23:28:12 -0500

    Update solution_9.py

--- ProjectEuler/solution_9.py
+++ ProjectEuler/solution_9.py
@@ -8,3 +8,16 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 
 '''
+
+for a in range(3, 1000):
+    for b in range (a + 1, 999):
+        cSquared = a**2 + b**2
+        c = cSquared**0.5
+
+        if a + b + c == 1000:
+            product = a * b * c
+            print(product)
+            break
+            
+            
+  # referance: https://code.mikeyaworski.com/python/project_euler/problem_9
