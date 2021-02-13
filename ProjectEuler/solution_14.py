0a756a99d52be20a74085aaceca9bccee20ecf26
Harun Yenial <harun.yenial@gmail.com>
2021-02-12 23:47:26 -0500

    Update solution_14.py

--- ProjectEuler/solution_14.py
+++ ProjectEuler/solution_14.py
@@ -14,3 +14,53 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 '''
 
+#http://radiusofcircle.blogspot.com
+
+#time module for execution time
+import time
+
+#Time at the start of execution
+start = time.time()
+
+#collatz function straight forward
+def collatz(n):
+ """
+ This function will take
+ a number and it will return
+ the size of the collatz
+ sequence for input n"""
+ counter = 1
+ while n>1:
+  if n%2 == 0:
+   n = n/2
+   counter += 1
+  else:
+   n = 3*n+1
+   counter += 1
+  if n == 1:
+   return counter
+
+#let the largest number be 0
+largest_number = 0
+
+#let the largest sequence size be 0
+large_seq = 0
+
+#for loop for 1 million
+for i in xrange(1000000,1,-1):
+ #size of collatz for the iteration
+ n = collatz(i)
+ #if size if greater than previous
+ #replace it with the new one
+ if n > large_seq:
+  largest_number = i
+  large_seq = n
+
+#printing the largest number
+print largest_number
+
+#time at the end of execution
+end = time.time()
+
+#Printing the total time taken
+print end-start
