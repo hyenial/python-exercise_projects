99eb23516a35adda67ade86b1b3d61a9cd2bedaf
Harun Yenial <harun.yenial@gmail.com>
2021-02-20 23:55:54 -0500

    Update 18.py

--- ProjectEuler/18.py
+++ ProjectEuler/18.py
@@ -1,3 +1,5 @@
+'''
+Question
 By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
 3
@@ -28,3 +30,5 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. 
 However, Problem 67, is the same challenge with a triangle containing one-hundred rows; 
 it cannot be solved by brute force, and requires a clever method! ;o)
+
+'''
