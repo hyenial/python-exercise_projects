835f4e1111ec6387d29ee7baa1d2fd2647e4fc02
Harun Yenial <harun.yenial@gmail.com>
2021-02-23 23:40:34 -0500

    Update 20.py

--- ProjectEuler/20.py
+++ ProjectEuler/20.py
@@ -5,3 +5,31 @@ For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum of the d
 
 Find the sum of the digits in the number 100!
 '''
+
+# solution wat 1
+#http://radiusofcircle.blogspot.com
+
+#importing factorial function from math
+from math import factorial
+
+#time module for execution time calculation
+import time
+
+#time at the start of execution
+start = time.time()
+
+#creating a list with the digits of factorial
+a = list(str(factorial(100)))
+
+#converting the string to int
+a = [int(x) for x in a]
+
+#sum of the numbers
+#factorial digit sum(fds)
+fds = sum(a)
+
+#time at the end of execution
+end = time.time()
+
+#print execution time and the solution
+print 'Found {} in {} seconds'.format(fds,end-start)
