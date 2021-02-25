e9f0c145d3dc44b95596481a9b309623261c7fee
Harun Yenial <harun.yenial@gmail.com>
2021-02-24 23:49:20 -0500

    Update 21.py

--- ProjectEuler/21.py
+++ ProjectEuler/21.py
@@ -8,3 +8,29 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 Evaluate the sum of all the amicable numbers under 10000.
 
 """
+def amicable_number(x):
+    amic_range = range(1, x)
+    amicable_numbers = []
+
+    for num in amic_range:
+
+        x_val = 0
+        y_val = 0
+        first_iter_list = range(1, num)
+        for val in first_iter_list:
+            if (num%val)==0:
+                x_val+=val
+
+        for val in range(1, x_val):
+            if (x_val%val)==0:
+                y_val+=val
+
+        if y_val==num:
+            amicable_numbers.append(y_val)
+
+    result = sum(amicable_numbers)
+
+    return result
+
+amicable_num_result = amicable_number(10000)
+
