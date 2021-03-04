d9264f769db8c221c16feca18f3e1072c29b17ac
Harun Yenial <harun.yenial@gmail.com>
2021-03-03 23:58:01 -0500

    Update 23.py

--- ProjectEuler/23.py
+++ ProjectEuler/23.py
@@ -1,4 +1,4 @@
-'''
+"""
 A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
 For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 
@@ -11,4 +11,4 @@ that cannot be expressed as the sum of two abundant numbers is less than this li
 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
-'''
+"""
