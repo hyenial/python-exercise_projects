559587e8cbca2b25c172dfb15aa70c9e8e95648b
Harun Yenial <harun.yenial@gmail.com>
2021-03-07 22:09:27 -0500

    Update 25.py

--- ProjectEuler/25.py
+++ ProjectEuler/25.py
@@ -23,3 +23,38 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
 """
+
+#http://radiusofcircle.blogspot.com
+
+#time module for execution time
+import time
+
+#time at the start of program
+start = time.time()
+
+#list to store the fibonacci numbers
+# added first two numbers of the 
+#fibonacci sequence
+fib = [0,1]
+
+#iterator
+i = 2
+
+#An infinite loop, breaks 
+#when the number is 1000 digits long
+while True:
+	#using the function given in question
+	fib_new = fib[i-1]+fib[i-2]
+	#Appending the new fibonacci to the list
+	fib.append(fib_new)
+	#condition to check for 1000 digits
+	if fib_new>10**999:
+		print i
+		break
+	i = i+1
+
+#time at the end of execution
+end = time.time()
+
+#printing the total time for execution
+print end-start
