e43e8a7bca16ba5a16629b310eeacad4a33483c8
Harun Yenial <harun.yenial@gmail.com>
2021-03-12 22:03:11 -0500

    Update 27.py

--- ProjectEuler/27.py
+++ ProjectEuler/27.py
@@ -1,1 +1,16 @@
 
+"""
+n2+n+41
+It turns out that the formula will produce 40 primes for the consecutive integer values 0≤n≤39. However,
+when n=40, 402+40+41=40(40+1)+41 is divisible by 41, and certainly when n=41, 412+41+41 is clearly divisible by 41.
+
+The incredible formula n2−79×n+1601 was discovered, which produces 80 primes for the consecutive values 0≤n≤79. 
+The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+n2+an+b, where |a|<1000 and |b|≤1000where |n| is the modulus/absolute value of ne.g. |11|=11 and |−4|=4
+Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, 
+starting with n=0.
+
+"""
