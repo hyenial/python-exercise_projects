e96f7e92c9f7b31e56d973af626ff1ca23b8b165
Harun Yenial <harun.yenial@gmail.com>
2021-03-29 23:25:30 -0400

    Update 32.py

--- ProjectEuler/32.py
+++ ProjectEuler/32.py
@@ -9,3 +9,41 @@ Find the sum of all products whose multiplicand/multiplier/product identity can 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 '''
 
+#http://radiusofcircle.blogspot.com
+
+#time module for calculating the execution time
+import time
+
+#time at the start of execution
+start = time.time()
+
+#We will store the products in a set to avoid duplicates
+products = set()
+
+#Digits from 1-9 which will be checking later
+to_be_checked = set('123456789')
+
+#for single digit multiplicand
+for i in xrange(9):
+	for j in xrange(999,9999):
+		s = str(i)+str(j)+str(i*j)
+		if len(s) == 9 and set(s) == to_be_checked:
+			products.add(i*j)
+		elif len(s) > 9:break
+
+#for double digit multiplicand
+for i in xrange(9,99):
+	for j in xrange(99,999):
+		s = str(i)+str(j)+str(i*j)
+		if len(s) == 9 and set(s) == to_be_checked:
+			products.add(i*j)
+		elif len(s)>9: break
+
+#printing the result
+print sum(products)
+
+#time at the end of execution
+end = time.time()
+
+#total time taken for execution
+print end - start
