5cd517837532a2e4bb669daa325520a06e75dff2
Harun Yenial <harun.yenial@gmail.com>
2021-04-02 23:19:09 -0400

    Update 36.py

--- ProjectEuler/36.py
+++ ProjectEuler/36.py
@@ -6,3 +6,30 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 
 '''
+#http://radiusofcircle.blogspot.com
+
+#time module for calculating execution time
+import time
+
+#time at the start of program execution
+start = time.time()
+
+#solution to add the values for each iteration
+solution = 0
+
+#looping through odd numbers
+for i in xrange(1,1000000,2):
+	#checking the base 10 number
+	if str(i) == str(i)[::-1]:
+		#checking the base 2 number
+		if bin(i)[2:] == bin(i)[2:][::-1]:
+			solution += i
+
+#printing the number of instances
+print solution
+
+#time at the end of program execution
+end = time.time()
+
+#printing the execution time
+print end - start
