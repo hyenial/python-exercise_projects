8b07e4871f2012938012eb3426a281640aeccc12
Harun Yenial <harun.yenial@gmail.com>
2021-04-11 00:02:05 -0400

    Update 39.py

--- ProjectEuler/39.py
+++ ProjectEuler/39.py
@@ -6,3 +6,38 @@ If p is the perimeter of a right angle triangle with integral length sides, a,b,
 For which value of p≤1000, is the number of solutions maximised?
 
 '''
+
+
+# http://radiusofcircle.blogspot.com
+
+# time module for calculating execution time
+import time
+
+# Counter method
+from collections import Counter
+
+# time at the start of program execution
+start = time.time()
+
+# list to store perimeters
+perimeters = []
+
+# looping to generate a,b and c
+for a in xrange(1, 500):
+    for b in xrange(a, 500):
+        c = (a**2 + b**2)**0.5
+        if int(c) == c and a + b + c <= 1000:
+            perimeters.append(a+b+c)
+
+# counting the instances of perimeters
+p = Counter(perimeters)
+
+# printing the most common perimeter
+# or printing the most repeated perimeter
+print p.most_common(1)[0]
+
+# time at the end of program execution
+end = time.time()
+
+# printing the total execution time
+print end - start
