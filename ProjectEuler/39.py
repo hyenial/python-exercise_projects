cab3beb32f5c6aeeedf5dda2642c9ce53dd46150
Harun Yenial <harun.yenial@gmail.com>
2021-04-10 23:58:20 -0400

    Update 39.py

--- ProjectEuler/39.py
+++ ProjectEuler/39.py
@@ -1,3 +1,8 @@
 '''
 
+If p is the perimeter of a right angle triangle with integral length sides, a,b,c, there are exactly three solutions for p=120.
+
+{20,48,52},{24,45,51},{30,40,50}
+For which value of p≤1000, is the number of solutions maximised?
+
 '''
