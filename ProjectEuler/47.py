f969f0137959d49ef7dd184612af2d9d190e3f89
Harun Yenial <harun.yenial@gmail.com>
2021-05-01 23:36:16 -0400

    Update 47.py

--- ProjectEuler/47.py
+++ ProjectEuler/47.py
@@ -15,3 +15,47 @@ Find the first four consecutive integers to have four distinct prime factors eac
 
 
 '''
+
+# http://radiusofcircle.blogspot.com
+
+# time module
+import time
+
+# time at the start of program execution
+start = time.time()
+
+
+def npf(number):
+    """function which will return
+    the number of prime factors"""
+    i = 2
+    a = set()
+    while i < number**0.5 or number == 1:
+        if number % i == 0:
+            number = number/i
+            a.add(i)
+            i -= 1
+        i += 1
+    return (len(a)+1)
+
+# iterator
+j = 2*3*5*7
+
+# while loop
+while True:
+    if npf(j) == 4:
+        j += 1
+        if npf(j) == 4:
+            j += 1
+            if npf(j) == 4:
+                j += 1
+                if npf(j) == 4:
+                    print j-3
+                    break
+    j += 1
+
+# time at the end of program execution
+end = time.time()
+
+# total time for execution
+print end - start
