1e148292f5e22ca0ef0d60e6ed7464db87873153
Harun Yenial <harun.yenial@gmail.com>
2021-05-06 19:41:07 -0400

    Update 49.py

--- ProjectEuler/49.py
+++ ProjectEuler/49.py
@@ -8,3 +8,69 @@ There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, ex
 What 12-digit number do you form by concatenating the three terms in this sequence?
 
 '''
+
+# http://radiusofcircle.blogspot.com
+
+# time module
+import time
+
+# importing permutations
+from itertools import permutations
+
+# time at the start of program execution
+start = time.time()
+
+
+# Sieve of Erotosthenes
+# One of the best algorithm to generate prime numbers
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+def sieve(n):
+    is_prime = [True]*n
+    is_prime[0] = False
+    is_prime[1] = False
+    is_prime[2] = True
+    # even numbers except 2 have been eliminated
+    for i in xrange(3, int(n**0.5+1), 2):
+        index = i*2
+        while index < n:
+            is_prime[index] = False
+            index = index+i
+    prime = [2]
+    for i in xrange(3, n, 2):
+        if is_prime[i]:
+            prime.append(i)
+    return prime
+
+
+# create the number
+def create(b):
+    for i in xrange(len(b)):
+        for j in xrange(i+1, len(b)):
+            difference = b[j] - b[i]
+            if b[j] + difference in b:
+                return str(b[i])+str(b[j])+str(b[j]+difference)
+    return False
+
+# sieving prime numbers less than 10000
+primes = sieve(10000)
+
+# primes greater than 1487
+primes = [x for x in primes if x > 1487]
+
+# for loop
+for i in primes:
+    p = permutations(str(i))
+    a = [int(''.join(x)) for x in p]
+    a = list(set([x for x in a if x in primes]))
+    a.sort()
+    if len(a) >= 3:
+        if create(a):
+            print create(a)
+            break
+
+# time at the end of program execution
+end = time.time()
+
+# total time for execution
+print end - start
+view raw
