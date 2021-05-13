1c389d135c60b124345df1900e3453f84b4024c4
Harun Yenial <harun.yenial@gmail.com>
2021-05-12 23:16:37 -0400

    Update 52.py

--- ProjectEuler/52.py
+++ ProjectEuler/52.py
@@ -8,3 +8,30 @@ Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain
 '''
 
 
+# http://radiusofcircle.blogspot.com
+
+# time module for calculating execution time
+import time
+
+# time at the start of program execution
+start = time.time()
+
+# while loop iterator
+i = 1
+
+# while loop
+while True:
+    if set(str(i)) == set(str(6*i)):
+        if set(str(i)) == set(str(5*i)):
+            if set(str(i)) == set(str(4*i)):
+                if set(str(i)) == set(str(3*i)):
+                    if set(str(i)) == set(str(2*i)):
+                        print i
+                        break
+    i += 1
+
+# time at the end of program execution
+end = time.time()
+
+# total time for program execution
+print end - start
