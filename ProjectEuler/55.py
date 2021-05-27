ca9f3c259d36d4b40f1706cd695a2a0e31541846
Harun Yenial <harun.yenial@gmail.com>
2021-05-26 22:17:08 -0400

    Update 55.py

--- ProjectEuler/55.py
+++ ProjectEuler/55.py
@@ -24,3 +24,41 @@ How many Lychrel numbers are there below ten-thousand?
 
 NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers.
 '''
+# http://radiusofcircle.blogspot.com
+
+# time module
+import time
+
+# time at the start of program execution
+start = time.time()
+
+
+def is_lychrel(n):
+    """this function will check if the number
+    is lychrel number or not"""
+    # fifty iterations
+    for i in xrange(50):
+        # sum of number and reverse
+        number = n + int(str(n)[::-1])
+        # if is palindrome
+        if str(number) == str(number)[::-1]:
+            return False
+        n = number
+    return True
+
+# counter to count
+counter = 0
+
+# looping till ten thousand
+for i in xrange(10001):
+    if is_lychrel(i):
+        counter += 1
+
+# printing the counter
+print counter
+
+# time at the end of program execution
+end = time.time()
+
+# total time of execution
+print end - start
