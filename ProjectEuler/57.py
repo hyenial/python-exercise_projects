6cde69461ffb0b27bcf7c5e67963b0d0a6e71844
Harun Yenial <harun.yenial@gmail.com>
2021-06-07 15:25:34 -0400

    Update 57.py

--- ProjectEuler/57.py
+++ ProjectEuler/57.py
@@ -1,3 +1,37 @@
 '''
 https://projecteuler.net/problem=57
 '''
+# http://radiusofcircle.blogspot.com
+
+# time module
+import time
+
+# time at the start of program execution
+start = time.time()
+
+# p
+p = 1
+
+# q
+q = 1
+
+# counter
+counter = 0
+
+# 1000 iterations
+for i in xrange(1000):
+    a1 = p + 2*q
+    b1 = p + q
+    if len(str(a1)) > len(str(b1)):
+        counter += 1
+    p = a1
+    q = b1
+
+# printing the counter
+print counter
+
+# time at the end of program execution
+end = time.time()
+
+# total time of execution
+print end - start
