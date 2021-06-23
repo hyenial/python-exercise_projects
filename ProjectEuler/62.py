ab1e58b45b15d5259970e96f2532e778131040a5
Harun Yenial <harun.yenial@gmail.com>
2021-06-23 17:56:56 -0400

    Update 62.py

--- ProjectEuler/62.py
+++ ProjectEuler/62.py
@@ -7,3 +7,35 @@ In fact, 41063625 is the smallest cube which has exactly three permutations of i
 Find the smallest cube for which exactly five permutations of its digits are cube.
 
 '''
+# http://radiusofcircle.blogspot.com
+
+# import time
+import time
+
+# time at the start of program
+start = time.time()
+
+# list to store cubes
+cubes = []
+
+# iterator
+i = 0
+
+# while loop
+while True:
+ # cube of the number
+ cube = sorted(list(str(i**3)))
+ # appending the cube to cubes list
+ cubes.append(cube)
+ # check if it occured 5 times
+ if cubes.count(cube) == 5:
+  # print the cube of the smallest such cube
+  print (cubes.index(cube))**3
+  break
+ i += 1
+
+# time at the end of program
+end = time.time()
+
+# total time taken
+print end - start
