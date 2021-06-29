28fb01e6752d38880a8df98ed7c7442fbcdd91ce
Harun Yenial <harun.yenial@gmail.com>
2021-06-28 23:07:33 -0400

    Update 64.py

--- ProjectEuler/64.py
+++ ProjectEuler/64.py
@@ -2,3 +2,20 @@
 https://projecteuler.net/problem=64
 
 '''
+
+
+# http://radiusofcircle.blogspot.com
+def continued_fraction(n):
+    """
+    This function will calculate
+    Continued fraction of a given number"""
+    integer = int(n)
+    decimal = n - integer
+    cf_list = [integer]
+    if decimal != 0:
+        while integer != 2*cf_list[0]:
+            decimal_inv = 1/decimal
+            integer = int(decimal_inv)
+            decimal = decimal_inv - integer
+            cf_list.append(integer)
+    return cf_list
