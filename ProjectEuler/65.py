5b3f31cebaf92e09fba523ca414927fdd194ec68
Harun Yenial <harun.yenial@gmail.com>
2021-07-02 23:11:48 -0400

    Update 65.py

--- ProjectEuler/65.py
+++ ProjectEuler/65.py
@@ -25,3 +25,41 @@ Find the sum of digits in the numerator of the 100<sup>th</sup> convergent of th
 
 
 '''
+# http://radiusofcircle.blogspot.com
+
+# time module
+import time
+
+# time at the start of program execution
+start = time.time()
+
+# exponent continued fraction
+e = [2,]
+
+# iterator
+i = 1
+
+# while loop to get 100 numbered list
+while len(e) < 100:
+    e.extend([1, 2*i, 1])
+    i += 1
+
+# initial numerator
+numerator = 1
+
+# last number of the list as denominator
+denominator = e.pop()
+
+# looping through the list to get the convergents
+for i in e[::-1]:
+    denominator, numerator =  (denominator * i + numerator, denominator)
+
+# finding the answer
+# remember denominator is the new numerator
+print sum([int(digit) for digit in str(denominator)])
+
+# time at the end of program execution
+end = time.time()
+
+# print total time taken
+print end - start
