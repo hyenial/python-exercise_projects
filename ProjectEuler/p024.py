f0866139bd7c48c25cda1206640c58ca0868a5a0
Harun Yenial <harun.yenial@gmail.com>
2021-08-03 22:32:44 -0400

    Update p024.py

--- ProjectEuler/p024.py
+++ ProjectEuler/p024.py
@@ -9,3 +9,32 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 
 """
 
+#http://radiusofcircle.blogspot.com
+
+#time for calculation of execution time
+import time
+
+#import permutations from itertools
+from itertools import permutations
+
+#time at the start of program execution
+start = time.time()
+
+#Generating Lexicographic permutations with python
+#converting the solution to a list
+#as it is returned as itertools.permutations object
+lexi_perm = list(permutations('0123456789'))
+
+#Each permutation is in the form of tuple
+#so join all the strings to get solution
+solution = ''.join(lexi_perm[999999])
+
+#printing the solution
+print solution
+
+#time at the end of execution
+end = time.time()
+
+#printing the total time taken
+print end - start
+
