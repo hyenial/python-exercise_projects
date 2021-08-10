8879a3bdd0e56262c20d86967dfb4776d6640e7a
Harun Yenial <harun.yenial@gmail.com>
2021-08-09 23:25:00 -0400

    Update p028.py

--- ProjectEuler/p028.py
+++ ProjectEuler/p028.py
@@ -14,3 +14,42 @@ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
 
 
 """
+
+#http://radiusofcircle.blogspot.com
+
+#time module for calculating the execution time
+import time
+
+#time at the start of program execution
+start = time.time()
+
+#last number of the maximum number
+last_number = 1001*1001
+
+#generating odd numbers list
+odd_numbers = xrange(1,last_number+1,2)
+
+#iterator the numbers
+i = 0
+
+#gap for every four iterations
+gap = 1
+
+#answer
+solution = 1
+
+#While loop to loop until we reach the last number
+while odd_numbers[i] != last_number:
+	for j in xrange(4):
+		i+= gap
+		solution += odd_numbers[i]
+	gap += 1
+
+#printing the solution
+print solution
+
+#time at the end of the program execution
+end = time.time()
+
+#printing the total time of execution
+print end - start
