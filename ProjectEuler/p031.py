412bd83496d1eecd45362dbaacbac6fc70af63aa
Harun Yenial <harun.yenial@gmail.com>
2021-08-12 22:15:13 -0400

    Update p031.py

--- ProjectEuler/p031.py
+++ ProjectEuler/p031.py
@@ -8,3 +8,38 @@ It is possible to make £2 in the following way:
 How many different ways can £2 be made using any number of coins?
 
 '''
+
+#http://radiusofcircle.blogspot.com
+
+#time module for calculating execution time
+import time
+
+#time at the start of the program execution
+start = time.time()
+
+#counter for couting number of possibilities
+counter = 0
+
+# number of 100p coins
+for a in xrange(3):
+	# number of 50p coins
+		for b in xrange(1+(200-100*a)/50):
+			# number of 20p coins
+				for c in xrange(1+(200-100*a-50*b)/20):
+					# number of 10p coins
+						for d in xrange(1+(200-100*a-50*b-20*c)/10):
+							# number of 5p coins
+								for e in xrange(1+(200-100*a-50*b-20*c-10*d)/5):
+									# number of 2p coins
+										for f in xrange(1+(200-100*a-50*b-20*c-10*d-5*e)/2):
+											counter += 1
+
+# Total number of ways we can form the 200p
+# Added 1 for 200p case
+print counter+1
+
+#time at the end of program execution
+end = time.time()
+
+#Total time taken for the program execution
+print end - start
