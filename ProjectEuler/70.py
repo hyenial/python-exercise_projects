cc7421b5faa0180619a70e54496a86af6ebe10f6
Harun Yenial <harun.yenial@gmail.com>
2021-08-18 23:15:51 -0400

    Update 70.py

--- ProjectEuler/70.py
+++ ProjectEuler/70.py
@@ -1,1 +1,33 @@
+'''
+Euler's Totient function, φ(n) [sometimes called the phi function], 
+is used to determine the number of positive numbers less than or equal to n which are relatively prime to n.
+For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+The number 1 is considered to be relatively prime to every positive number, so φ(1)=1.
+
+Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation of 79180.
+
+Find the value of n, 1 < n < 107, for which φ(n) is a permutation of n and the ratio n/φ(n) produces a mini
+
+
+
+<html>
+<p>Euler's Totient function, φ(<var>n</var>) [sometimes called the phi function], 
+is used to determine the number of positive numbers less than or equal to <var>n</var> 
+which are relatively prime to <var>n</var>. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.<br />T
+he number 1 is considered to be relatively prime to every positive number, so φ(1)=1. </p>
+<p>Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation of 79180.</p>
+<p>Find the value of <var>n</var>, 1 &lt; <var>n</var> &lt; 10<sup>7</sup>, for which φ(<var>n</var>) 
+is a permutation of <var>n</var> and the ratio <var>n</var>/φ(<var>n</var>) produces a minimum.</p>
+</html>
+
+'''
+
+
+
+
+
+
+
+
+
 
