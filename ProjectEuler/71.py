dce68c3b20f3e87cc816baff452972461e526765
Harun Yenial <harun.yenial@gmail.com>
2021-08-28 23:45:36 -0400

    Update 71.py

--- ProjectEuler/71.py
+++ ProjectEuler/71.py
@@ -10,3 +10,31 @@ It can be seen that 2/5 is the fraction immediately to the left of 3/7.
 By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, 
 find the numerator of the fraction immediately to the left of 3/7.
 """
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+t = int(input())
+
+for i in range(t):
+    a, b, n = [int(x) for x in input().split()]
+    inv_a = modinv(a, b)
+    r = n % b
+    d = r - inv_a
+    if d < 0:
+        d += b
+    den = n - d
+    num = (den * a - 1) // b
+    print("{} {}".format(num, den))
