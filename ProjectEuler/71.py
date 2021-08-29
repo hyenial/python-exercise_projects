3c081d4bd357843833c4851d18fa2955cf29e494
Harun Yenial <harun.yenial@gmail.com>
2021-08-28 23:47:32 -0400

    Update 71.py

--- ProjectEuler/71.py
+++ ProjectEuler/71.py
@@ -38,3 +38,21 @@ for i in range(t):
     den = n - d
     num = (den * a - 1) // b
     print("{} {}".format(num, den))
+    
+    
+ # solution 2
+def compute():
+	LIMIT = 1000000
+	maxnumer = 0
+	maxdenom = 1
+	for d in range(1, LIMIT + 1):
+		n = d * 3 // 7
+		if d % 7 == 0:
+			n -= 1
+		if n * maxdenom > d * maxnumer:  # n/d > maxdenom/maxnumer
+			maxnumer = n
+			maxdenom = d
+	return str(maxnumer)
+
+
+if __name__ == "__main_
