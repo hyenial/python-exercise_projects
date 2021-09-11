b76bc2e42485c99d936ca8b8f8083309914f30f4
Harun Yenial <harun.yenial@gmail.com>
2021-09-10 23:38:54 -0400

    Update 73.py

--- ProjectEuler/73.py
+++ ProjectEuler/73.py
@@ -10,3 +10,17 @@ It can be seen that there are 3 fractions between 1/3 and 1/2.
 How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d ≤ 12,000?
 
 '''
+
+#Project Euler Problem 73
+
+def ceildiv(a, b): return -(-a // b)
+
+def p(L, n1=1, d1=3,  n2=1, d2=2):
+    n = [0] * (L+1) 
+    for d in xrange(1, L+1):
+        n[d] += ceildiv(n2*d, d2) - ceildiv(n1*d, d1) - 1
+        n[2*d::d] = [k-n[d] for k in n[2*d::d]]
+    return sum(n)
+
+print p(12000), "fractions found in range."
+
