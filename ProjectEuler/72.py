fab6566d30069ed6ac3a61b44f115c140627b1a9
Harun Yenial <harun.yenial@gmail.com>
2021-09-10 23:38:40 -0400

    Update 72.py

--- ProjectEuler/72.py
+++ ProjectEuler/72.py
@@ -10,15 +10,3 @@ It can be seen that there are 21 elements in this set.
 How many elements would be contained in the set of reduced proper fractions for d ≤ 1,000,000?
 
 """
-#Project Euler Problem 73
-
-def ceildiv(a, b): return -(-a // b)
-
-def p(L, n1=1, d1=3,  n2=1, d2=2):
-    n = [0] * (L+1) 
-    for d in xrange(1, L+1):
-        n[d] += ceildiv(n2*d, d2) - ceildiv(n1*d, d1) - 1
-        n[2*d::d] = [k-n[d] for k in n[2*d::d]]
-    return sum(n)
-
-print p(12000), "fractions found in range."
