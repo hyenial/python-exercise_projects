bb1695991ab8f1244406311d81d4fcd0ffe79db3
Harun Yenial <harun.yenial@gmail.com>
2021-09-12 19:55:57 -0400

    Update 72.py

--- ProjectEuler/72.py
+++ ProjectEuler/72.py
@@ -10,3 +10,32 @@ It can be seen that there are 21 elements in this set.
 How many elements would be contained in the set of reduced proper fractions for d ≤ 1,000,000?
 
 """
+
+
+def solution(limit: int = 1000000) -> int:
+    """
+    Return the number of reduced proper fractions with denominator less than limit.
+    >>> solution(8)
+    21
+    >>> solution(1000)
+    304191
+    """
+    primes = set(range(3, limit, 2))
+    primes.add(2)
+    for p in range(3, limit, 2):
+        if p not in primes:
+            continue
+        primes.difference_update(set(range(p * p, limit, p)))
+
+    phi = [float(n) for n in range(limit + 1)]
+
+    for p in primes:
+        for n in range(p, limit + 1, p):
+            phi[n] *= 1 - 1 / p
+
+    return int(sum(phi[2:]))
+
+
+if __name__ == "__main__":
+    print(f"{solution() = }")
+
