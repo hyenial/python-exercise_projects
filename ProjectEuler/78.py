c6c7f32142c817d3081d4a9c7c233b9d739667cf
Harun Yenial <harun.yenial@gmail.com>
2021-11-03 23:56:30 -0400

    Update 78.py

--- ProjectEuler/78.py
+++ ProjectEuler/78.py
@@ -1,4 +1,4 @@
-'''
+"""
 Let p(n) represent the number of different ways in which n coins can be separated into piles. 
 For example, five coins can be separated into piles in exactly seven different ways, so p(5)=7.
 
@@ -11,4 +11,4 @@ OO   O   O   O
 O   O   O   O   O
 Find the least value of n for which p(n) is divisible by one million.
 
-'''
+"""
