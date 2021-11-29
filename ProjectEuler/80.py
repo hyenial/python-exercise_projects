292b69583e2766949b8e1b86879b6db5f79aaf32
Harun Yenial <harun.yenial@gmail.com>
2021-11-28 22:10:21 -0500

    Update 80.py

--- ProjectEuler/80.py
+++ ProjectEuler/80.py
@@ -10,3 +10,25 @@ For the first one hundred natural numbers,
 find the total of the digital sums of the first one hundred decimal digits for all the irrational square roots.
 
 '''
+#time
+
+let nums  = Set [ 1I; 4I; 9I; 16I; 25I; 36I; 49I; 64I; 81I; 100I ] 
+            |> Set.difference (Set [| 1I..100I |])
+let limit = bigint.Pow(10I, 101)
+
+let squareRoot n =
+    let rec loop a b =
+        if b >= limit then b
+        elif a >= b then loop (a - b) (b + 10I)
+        else loop (a * 100I) (b / 10I * 100I + 5I)
+
+    loop (5I * n) 5I
+
+let zero = int '0'
+let sum (n : bigint) = 
+    n.ToString() 
+    |> Seq.take 100
+    |> Seq.sumBy (fun char -> int char - zero)
+
+let answer = nums |> Seq.sumBy (squareRoot >> sum)
+#referance https://github.com/theburningmonk/ProjectEuler-FSharp-Solutions/blob/master/ProjectEulerSolutions/ProjectEulerSolutions/Problem080.fsx
