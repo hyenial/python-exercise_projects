b6a5f0cc2e98eae52300513a3d1fa7b54e4ca30c
Harun Yenial <harun.yenial@gmail.com>
2021-12-03 23:28:51 -0500

    Update 81.py

--- ProjectEuler/81.py
+++ ProjectEuler/81.py
@@ -6,5 +6,5 @@ In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom
 Find the minimal path sum from the top left to the bottom right by only moving right and down in matrix.txt (right click and "Save Link/Target As..."), 
 a 31K text file containing an 80 by 80 matrix.
 
-https://projecteuler.net/project/resources/p081_matrix.txt
+# https://projecteuler.net/project/resources/p081_matrix.txt
 '''
