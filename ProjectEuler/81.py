b50e2055ec0ef2235d29f321e31da7acda4202ec
Harun Yenial <harun.yenial@gmail.com>
2021-12-04 23:57:11 -0500

    Update 81.py

--- ProjectEuler/81.py
+++ ProjectEuler/81.py
@@ -7,4 +7,15 @@ Find the minimal path sum from the top left to the bottom right by only moving r
 a 31K text file containing an 80 by 80 matrix.
 
 # https://projecteuler.net/project/resources/p081_matrix.txt
+
 '''
+m = [map(int, row.split(',')) for row in open("tps://projecteuler.net/project/resources/p081_matrix.txt").readlines()]
+c, r = len(m), len(m[0])
+
+for i in xrange(1,c):
+    for j in xrange(1,r):
+        m[i][0]+= m[i-1][0]
+        m[0][j]+= m[0][j-1]
+        m[i][j]+= min(m[i-1][j], m[i][j-1])
+ 
+print "Minimal path sum in", r, "by", c, "matrix =", m[-1][-1]
