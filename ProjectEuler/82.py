d8af80667f1f731f1f69c77d8ee49839e86a701e
Harun Yenial <harun.yenial@gmail.com>
2021-12-06 23:57:27 -0500

    Update 82.py

--- ProjectEuler/82.py
+++ ProjectEuler/82.py
@@ -1,1 +1,11 @@
 
+'''
+
+The minimal path sum in the 5 by 5 matrix below, by starting in any cell in the left column and finishing in any cell in the right column, 
+and only moving up, down, and right, is indicated in red and bold; the sum is equal to 994.
+
+ 
+Find the minimal path sum from the left column to the right column in matrix.txt (https://projecteuler.net/project/resources/p081_matrix.txt."),
+a 31K text file containing an 80 by 80 matrix.
+
+'''
