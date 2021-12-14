547a02f0ae99a2e66d9ba315bde8397c6b3208c3
Harun Yenial <harun.yenial@gmail.com>
2021-12-13 20:00:41 -0500

    Update 83.py

--- ProjectEuler/83.py
+++ ProjectEuler/83.py
@@ -1,1 +1,7 @@
+'''
+In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, 
+by moving left, right, up, and down, is indicated in bold red and is equal to 2297.
 
+ 
+Find the minimal path sum from the top left to the bottom right by moving left, right, up, and down in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing an 80 by 80 matrix.
+'''
