a984520a072d180f1b65b2ca1209662e87cbc92a
Harun Yenial <harun.yenial@gmail.com>
2022-01-09 22:39:52 -0500

    Create passwordGenerator.py

---
+++ practice/passwordGenerator.py
@@ -0,0 +1,12 @@
+
+# generate a password with length "passlen" with no duplicate characters in the password
+
+import random
+
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = 8
+p =  "".join(random.sample(s,passlen ))
+print p
+
+
+# https://www.practicepython.org/solution/2014/06/06/16-password-generator-solutions.html
