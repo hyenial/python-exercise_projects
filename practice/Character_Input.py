f92dc597c0e250679c52d65b6c1fd9e99ddac288
Harun Yenial <harun.yenial@gmail.com>
2022-01-11 23:26:04 -0500

    Create Character_Input.py

---
+++ practice/Character_Input.py
@@ -0,0 +1,10 @@
+'''
+Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+'''
+
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2014 - age)+100)
+print(name + " will be 100 years old in the year " + year)
