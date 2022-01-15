533f1075db754ca9cf2b25e3c7da0a6bd21fdadf
Harun Yenial <harun.yenial@gmail.com>
2022-01-14 23:56:54 -0500

    Create divisor.py

---
+++ practice/divisor.py
@@ -0,0 +1,20 @@
+"""
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+(If you don’t know what a divisor is, it is a number that divides evenly into another number.For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+
+"""
+
+__author__ = 'jeffreyhunt'
+
+num = int(input("Please choose a number to divide: "))
+
+listRange = list(range(1,num+1))
+
+divisorList = []
+
+for number in listRange:
+    if num % number == 0:
+        divisorList.append(number)
+
+print(divisorList)
