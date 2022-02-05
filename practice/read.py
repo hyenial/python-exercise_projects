3ff3a629a3a558d5bcc36554a434120eed54a565
Harun Yenial <harun.yenial@gmail.com>
2022-02-04 23:46:40 -0500

    Create read.py

---
+++ practice/read.py
@@ -0,0 +1,12 @@
+counter_dict = {}
+with open('Training_01.txt') as f:
+	line = f.readline()
+	while line:
+		line = line[3:-26]
+		if line in counter_dict:
+			counter_dict[line] += 1
+		else:
+			counter_dict[line] = 1
+		line = f.readline()
+
+print(counter_dict)
