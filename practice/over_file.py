51865f6113006a818d9628f5236641aab581b2c8
Harun Yenial <harun.yenial@gmail.com>
2022-02-05 22:48:26 -0500

    Create over_file.py

---
+++ practice/over_file.py
@@ -0,0 +1,20 @@
+primeslist = []
+with open('primenumbers.txt') as primesfile:
+	line = primesfile.readline()
+	while line:
+		primeslist.append(int(line))
+		line = primesfile.readline()
+
+happieslist = []
+with open('happynumbers.txt') as happiesfile:
+	line = happiesfile.readline()
+	while line:
+		happieslist.append(int(line))
+		line = happiesfile.readline()
+
+overlaplist = []
+for elem in primeslist:
+	if elem in happieslist:
+		overlaplist.append(elem)
+		
+print(overlaplist)
