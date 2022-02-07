62b3bad8760cc99e6359fe35e4d7c884ca5ab0c6
Harun Yenial <harun.yenial@gmail.com>
2022-02-06 23:31:46 -0500

    Create board.py

---
+++ practice/board.py
@@ -0,0 +1,12 @@
+def drawboard(kamal):
+    kamal = int(kamal)
+    i = 0
+    ho = "--- "
+    ve = "|   "
+    ho = ho * kamal
+    ve = ve * (kamal+1)
+    while i < kamal+1:
+        print ho
+        if not (i == kamal):
+            print ve
+        i += 1
