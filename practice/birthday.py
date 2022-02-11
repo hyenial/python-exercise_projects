9d407baa5222f59f17a3159b2ea218bd138f2ef3
Harun Yenial <harun.yenial@gmail.com>
2022-02-10 23:57:07 -0500

    Create birthday.py

---
+++ practice/birthday.py
@@ -0,0 +1,20 @@
+
+if __name__ == '__main__':
+
+    birthdays = {
+        'Albert Einstein': '03/14/1879',
+        'Benjamin Franklin': '01/17/1706',
+        'Ada Lovelace': '12/10/1815',
+        'Donald Trump': '06/14/1946',
+        'Rowan Atkinson': '01/6/1955'}
+
+    print('Welcome to the birthday dictionary. We know the birthdays of:')
+    for name in birthdays:
+        print(name)
+
+    print('Who\'s birthday do you want to look up?')
+    name = input()
+    if name in birthdays:
+        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
