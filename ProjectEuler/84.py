ee0d114fac17ea2fc1cf60a12ad2cf1116769969
Harun Yenial <harun.yenial@gmail.com>
2022-02-11 23:58:57 -0500

    Update 84.py

--- ProjectEuler/84.py
+++ ProjectEuler/84.py
@@ -25,7 +25,7 @@
 </ul><p>The heart of this problem concerns the likelihood of visiting a particular square. That is, the probability of finishing at that square after a roll. For this reason it should be clear that, with the exception of G2J for which the probability of finishing on it is zero, the CH squares will have the lowest probabilities, as 5/8 request a movement to another square, and it is the final square that the player finishes at on each roll that we are interested in. We shall make no distinction between "Just Visiting" and being sent to JAIL, and we shall also ignore the rule about requiring a double to "get out of jail", assuming that they pay to get out on their next turn.</p>
 <p>By starting at GO and numbering the squares sequentially from 00 to 39 we can concatenate these two-digit numbers to produce strings that correspond with sets of squares.</p>
 <p>Statistically it can be shown that the three most popular squares, in order, are JAIL (6.24%) = Square 10, E3 (3.18%) = Square 24, and GO (3.09%) = Square 00. So these three most popular squares can be listed with the six-digit modal string: 102400.</p>
-<p>If, instead of using two 6-sided dice, two 4-sided dice are used, find the six-digit modal string.</p>
+<p>If, instead of using two 6-sided dice, two 4-sided dice are used, find the six-digit modal string...</p>
 
 
 '''
